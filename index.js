e239d5dc9401b073c3fa8ac744f1c22e6fc5c6e5
anait31 <anaitullah198@gmail.com>
2024-11-26 21:20:44 +0800

    Implement update operation

--- index.js
+++ index.js
@@ -33,12 +33,36 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.findOne(query)
+            res.send(result);
+        })
+
 
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
             const result = await userCollection.insertOne(user);
             res.send(result);
+        })
+
+        app.put('/users/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedUser = req.body
+            const user = {
+                $set: {
+                    name: updatedUser.name,
+                    email: updatedUser.email,
+                    country: updatedUser.country
+                }
+            }
+            const result = await userCollection.updateOne(filter, user, options);
+            res.send(result)
+
         })
 
         app.delete('/users/:id', async (req, res) => {
