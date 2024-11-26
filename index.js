b6cd4f7d66d5958d43d3d9d4b2dffb6cce8dae5d
anait31 <anaitullah198@gmail.com>
2024-11-26 20:20:32 +0800

    Implement delete operation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const epxress = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = epxress()
 const port = process.env.PORT || 5000;
 
@@ -27,17 +27,25 @@ async function run() {
 
         const userCollection = client.db("userDB").collection('allUsers');
 
-        app.get('/addUsers', async(req, res) => {
+        app.get('/users', async (req, res) => {
             const users = userCollection.find();
             const result = await users.toArray();
             res.send(result)
         })
 
 
-        app.post('/addUsers', async(req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
             const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
